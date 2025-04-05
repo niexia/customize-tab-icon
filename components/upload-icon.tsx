6f572d2f7ec1f07bb46cf8d05962f34b9197b157
yangjin <yangjin.im@bytedance.com>
2025-04-05 22:28:13 +0800

    feat(ui): add label component and integrate into upload icon

--- components/upload-icon.tsx
+++ components/upload-icon.tsx
@@ -1,11 +1,47 @@
 import { Image } from "lucide-react";
-import { ActionItem } from "./action-item"
+import { ActionItem } from "./action-item";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { useState } from "react";
+import { Button } from "./ui/button";
 
 export function UploadIcon() {
+  const [open, setOpen] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = () => {
+    if (!inputValue?.trim()) {
+      return;
+    }
+    console.log("Submitted text:", inputValue);
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    setInputValue("");
+  }, [open])
+
   return (
-    <ActionItem>
-      <Image size={20}  />
-      <span className="font-medium">Upload</span>
-    </ActionItem>
-  )
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger className="w-full">
+        <ActionItem>
+        <Image size={20}  />
+        <span className="font-medium">Upload</span>
+      </ActionItem>
+      </PopoverTrigger>
+      <PopoverContent className="min-w-80 space-y-4" sideOffset={0} align="start" alignOffset={0}>
+        <div className="grid w-full lg:max-w-sm items-center gap-1.5">
+          <Label htmlFor="picture">Select a picture</Label>
+          <Input
+            id="picture"
+            type="file"
+          />
+        </div>
+        <Button className="w-full" onClick={handleSubmit}>
+          Confirm
+        </Button>
+      </PopoverContent>
+    </Popover>
+  );
 }
